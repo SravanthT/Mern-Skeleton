fecf502bc5bdda66c0ea7448b63d6eff0b67b1f4
Sravanth_T <sravanthtagore44+git@gmail.com>
2023-01-30 09:05:41 +0530

    user route methods updated with repective logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,27 +3,82 @@ import User from '../models/user.model';
 import extend from 'lodash/extend';
 import errorHandler from './error.controller';
 
-const create = (req,res,next)=>{
-
+const create = async (req,res,next)=>{
+    const user = new User(req.body)
+    try{
+        await user.save()
+        return res.status(200).json({
+            message: "Successfully signed up!"
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
-const list = (req,res) =>{
-
+const list = async (req,res) =>{
+    try{
+        let users = await User.find().select('name email updated created')
+        res.json(users)
+    }catch (err){
+        return res.status(400).json({
+            error : errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
-const userById = (req,res,next,id) =>{
-
+const userById = async (req,res,next,id) =>{
+    try{
+        let user = await User.findById(id)
+        if(!user)
+            return res.status('400').json({
+                error: "User not found"
+            })
+        req.profile = user
+        next()
+        
+    }catch(err){
+        return res.status('400').json({
+            error: "Could not retrieve user"
+        })
+    }
 }
 
 const read = (req,res) =>{
-
+    req.profile.hasher_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile)
 }
 
-const update = (req,res,next) =>{
-
+const update = async (req,res) =>{
+    try {
+    let user = req.profile;
+    user = extend(user,req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hasher_password = undefined;
+    user.salt = undefined;
+    res.json(user)
+    } catch(err){
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
-const remove = (req,res,next) =>{
+const remove = async (req,res,next) =>{
+    try{
+        let user = req.profile
+        let deletedUser = await user.remove();
+        deletedUser.hasher_password = undefined
+        deletedUser.salt = undefined
+        res.json(deletedUser)
+    }catch(err){
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 
 }
 
